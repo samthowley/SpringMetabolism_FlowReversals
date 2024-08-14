8a2ca21062fc69ff0903113e00046b08d5ba2275
Sam Howley <samanthahowley@ufl.edu>
2024-08-14 14:07:47 -0400

    improved reduction code

--- 03_Scripts/analysis/reduction_08062024.R
+++ 03_Scripts/analysis/reduction_08062024.R
@@ -1,5 +1,8 @@
 rm(list=ls())
 
+# check<-filter(OtFR,RI==2 )
+# ggplot(check, aes(Date))+geom_line(aes(y=ER))
+
 ##packages######
 library(ggpubr)
 library(readxl)
@@ -9,7 +12,7 @@ library(dataRetrieval)
 library(ggpmisc)
 library(tidyverse)
 
-extract_reduce <- function(site) {
+baseline <- function(site) {
   site<-site %>% filter(RI==2)%>%select(Date, DO, depth, ER, GPP, depth_diff, depthID, ID)
   
   site <- site %>% group_by(count = cumsum(c(TRUE, diff(Date) >= 1))) %>%ungroup()
@@ -20,6 +23,14 @@ extract_reduce <- function(site) {
   (ER_prior<-mean(site_prior$ER, na.rm=T))
   (h_prior<-mean(site_prior$depth, na.rm=T))
   
+  return(list(h_prior,ER_prior,GPP_prior))}
+extract_reduce <- function(site, baseline) {
+  
+  h_prior<-baseline[1][[1]]
+  ER_prior<-baseline[2][[1]]
+  GPP_prior<-baseline[3][[1]]
+  
+  site<-site %>% filter(RI==2)%>%select(Date, DO, depth, ER, GPP, depth_diff, depthID, ID)
   
   h<-max(site$depth_diff, na.rm = T)
   ER<-min(site$ER, na.rm = T)
@@ -70,37 +81,13 @@ extract_reduce_mod <- function(siteFR) {
   colnames(df)[7]<-'date'
   
   return(df)}
-extract_reduce_dbl <- function(site, site_prior) {
-  site<-site %>% filter(RI==2)%>%select(Date, DO, depth, ER, GPP, depth_diff, depthID, ID)
-  
-  site <- site %>% group_by(count = cumsum(c(TRUE, diff(Date) >= 1))) %>%ungroup()
-  
-  (GPP_prior<-mean(site_prior$GPP, na.rm=T))
-  (ER_prior<-mean(site_prior$ER, na.rm=T))
-  (h_prior<-mean(site_prior$depth, na.rm=T))
-  
-  
-  h<-max(site$depth_diff, na.rm = T)
-  ER<-min(site$ER, na.rm = T)
-  GPP<-min(site$GPP, na.rm = T)
-  date<-min(site$Date, na.rm = T)
-  
-  site_ls<-list(h_prior,ER_prior,GPP_prior,h,GPP,ER,date)
-  df<- data.frame(site_ls[[1]],site_ls[[2]],site_ls[[3]],
-                  site_ls[[4]],site_ls[[5]],site_ls[[6]],
-                  site_ls[[7]])
-  colnames(df)[1]<-'h0'
-  colnames(df)[2]<-'ER0'
-  colnames(df)[3]<-'GPP0'
-  colnames(df)[4]<-'h'
-  colnames(df)[5]<-'GPP'
-  colnames(df)[6]<-'ER'
-  colnames(df)[7]<-'date'
-  
-  return(df)}
 
+#Data##########
 master<-read_csv('02_Clean_data/master_metabolism4.csv')
-master<-master %>%group_by(ID) %>% mutate(depth_min=min(depth, na.rm=T))
+
+master<-master %>%
+  group_by(ID) %>% mutate(depth_min=min(depth, na.rm=T))
+
 master$depth_diff<-master$depth-master$depth_min
 
 IDs<-split(master,master$ID)
@@ -111,27 +98,27 @@ IU<-IDs[[4]]
 LF<-IDs[[5]]
 OS<-IDs[[6]]
 
-##########################
-#GB Reduction#############
-#########################
+#GB Reduction######################################
 
 GB<- GB %>% mutate(depthID = case_when(
   depth<0.55  ~ "low",
   depth>0.55 & depth<=0.75 ~ "moderate",
   depth>=0.75~ "high"))
 
-GBFR<- GB %>% mutate(RI = case_when(Date<"2023-01-30"~ 2))
-GBFR_102023<-extract_reduce(GBFR)
+GB_102023<- GB %>% mutate(RI = case_when(Date<"2023-01-30"~ 2))
+base_102023<-baseline(GB_102023)
+(GBFR_102023<-extract_reduce(GB_102023,base_102023))
 
-GBFR<- GB %>% mutate(RI = case_when(Date> "2023-05-16" & Date<"2023-10-31"~ 2))
-GBFR_082023<-extract_reduce(GBFR)
+GB_082023<- GB %>% mutate(RI = case_when(Date> "2023-05-16" & Date<"2023-10-31"~ 2))
+base_082023<-baseline(GB_082023)
+(GBFR_082023<-extract_reduce(GB_082023,base_082023))
 
-GBFR<- GB %>% mutate(RI = case_when(Date> "2023-09-16" & Date<"2024-05-11"~ 2))
-GBFR_012024<-extract_reduce(GBFR)
+GB_012024<- GB %>% mutate(RI = case_when(Date> "2023-09-16" & Date<"2024-05-11"~ 2))
+base_012024<-baseline(GB_012024)
+(GBFR_012024<-extract_reduce(GB_012024,base_012024))
 
 # GBFRcheck<-filter(GBFR,RI==2 )
 # ggplot(GB, aes(Date)) + geom_line(aes(y=GPP, color=depthID))+geom_hline(yintercept = 0.75)
-
 
 GB_tbl<-rbind(GBFR_102023,GBFR_082023,GBFR_012024)
 GB_tbl$ID<-'GB'
@@ -139,9 +126,7 @@ GB_tbl$num<-4
 GB_tbl$IF <- c("h",'h','rev')
 
 
-##########################
-#OS RI Reduction#############
-#########################
+#OS RI Reduction######################################
 
 OS<- OS %>% mutate(depthID = case_when(
   depth<0.8 ~ "low",
@@ -149,28 +134,41 @@ OS<- OS %>% mutate(depthID = case_when(
   depth>=1 ~ "high"))
 
 OtFR<- OS %>% mutate(RI = case_when(Date> "2022-08-10" & Date< "2023-01-01"~ 2))
-OSFR_092023<-extract_reduce(OtFR)
+base<-baseline(OtFR)
+OSFR_092023<-extract_reduce(OtFR,base) #bo
 
 OtFR<- OS %>% mutate(RI = case_when(Date> "2023-01-01" & Date<"2023-05-20"~ 2))
-OSFR_032023<-extract_reduce(OtFR)
+base<-baseline(OtFR)
+OSFR_032023<-extract_reduce(OtFR,base) #rev
 
 OtFR<- OS %>% mutate(RI = case_when(Date> "2023-05-20" & Date<"2023-12-10"~ 2))
-OSFR_072023<-extract_reduce(OtFR)
+base<-baseline(OtFR)
 
-OtFR<- OS %>% mutate(RI = case_when(Date> "2023-10-30" ~ 2))
-OSFR_042024<-extract_reduce(OtFR)
+OtFR_092023<-OtFR %>% mutate(RI = case_when(Date>"2023-08-20" & Date<"2023-10-15"~ 2))
+OtFR_092023<-extract_reduce(OtFR_092023,base) #bo
+
+
+OtFR<- OS %>% mutate(RI = case_when(Date> "2023-11-20" ~ 2))
+base<-baseline(OtFR)
+
+OtFR<- OS %>% mutate(RI = case_when(Date> "2023-11-20" & Date<'2024-03-10' ~ 2))
+OtFR_012024<-extract_reduce(OtFR,base) #rev
+
+OtFR<- OS %>% mutate(RI = case_when(Date>'2024-03-10'& Date<'2024-05-17' ~ 2)) 
+OtFR_042024<-extract_reduce(OtFR,base) #bo
+
+OtFR<- OS %>% mutate(RI = case_when(Date>'2024-05-17' ~ 2)) 
+OtFR_052024<-extract_reduce(OtFR,base) #bo
 
 # OSFRcheck<-filter(OtFR,RI==2 )
-# ggplot(OSFRcheck, aes(Date)) +geom_line(aes(y=depth, color=depthID))
+# ggplot(OSFRcheck, aes(Date))+geom_line(aes(y=ER))
 
-OS_tbl<-rbind(OSFR_092023,OSFR_032023,OSFR_072023,OSFR_042024)
+OS_tbl<-rbind(OSFR_092023,OSFR_032023,OtFR_092023,OtFR_012024,OtFR_042024,OtFR_052024)
 OS_tbl$ID<-'OS'
 OS_tbl$num<-5
-OS_tbl$IF <- c("rev",'bo','bo','rev')
+OS_tbl$IF <- c('bo','rev','bo','rev','bo','bo')
 
-##########################
-#OS mod Reduction#############
-#########################
+#OS mod Reduction######################################
 
 OSmod<-OS %>% filter(depthID != "high")
 
@@ -191,44 +189,54 @@ OSmod_tbl$IF <- c("h",'h')
 
 
 
-##########################
-#AM RI Reduction#############
-###########################
+#AM RI Reduction########################################
 
 AM<- AM %>% mutate(depthID = case_when(
   depth<0.9 ~ "low",
   depth>0.9 & depth<1.2 ~ "moderate",
   depth>=1.2 ~ "high"))
 
-AMFR<- AM %>% mutate(RI = case_when(Date< "2022-08-20"~ 2))
-AMFR_072022<-extract_reduce(AMFR)
+AMFR<- AM %>% mutate(RI = case_when(Date< "2022-08-27"~ 2))
+base<-baseline(AMFR)
+AMFR_072022<-extract_reduce(AMFR,base) #bo
 
-AMFR<- AM %>% mutate(RI = case_when(Date> "2022-08-17" & Date< "2022-11-20"~ 2))
-AMFR_092022<-extract_reduce(AMFR)
+AMFR<- AM %>% mutate(RI = case_when(Date> "2022-08-01" & Date< "2022-11-20"~ 2))
+AMFR_092022<-extract_reduce(AMFR,base) #bo
 
 AMFR<- AM %>% mutate(RI = case_when(Date> "2023-01-01" & Date< "2023-04-20"~ 2))
-AMFR_022023<-extract_reduce(AMFR)
+base<-baseline(AMFR)
+AMFR_022023<-extract_reduce(AMFR,base) #rev
 
 AMFR<- AM %>% mutate(RI = case_when(Date> "2023-05-20" & Date< "2023-09-01"~ 2))
-AMFR_072023<-extract_reduce(AMFR)
+base<-baseline(AMFR)
+AMFR_072023<-extract_reduce(AMFR,base) #bo
 
 AMFR<- AM %>% mutate(RI = case_when(Date> "2023-08-01" & Date< "2023-10-01"~ 2))
-AMFR_092023<-extract_reduce(AMFR)
+base<-baseline(AMFR)
+AMFR_092023<-extract_reduce(AMFR,base) #bo
+
+
 
 AMFR<- AM %>% mutate(RI = case_when(Date> "2023-11-01"~ 2))
-AMFR_012024<-extract_reduce(AMFR)
+base<-baseline(AMFR)
 
-AMFRcheck<-filter(AMFR,RI==2 )
-ggplot(AMFRcheck, aes(Date)) +geom_line(aes(y=SpC, color=depthID))
+AMFR<- AM %>% mutate(RI = case_when(Date> "2023-11-01" & Date< "2024-03-01"~ 2))
+AMFR_012024<-extract_reduce(AMFR, base) #rev
 
-AM_tbl<-rbind(AMFR_072022,AMFR_092022,AMFR_022023,AMFR_072023,AMFR_092023,AMFR_012024)
+AMFR<- AM %>% mutate(RI = case_when(Date>"2024-03-01" & Date<"2024-05-15"~ 2))
+AMFR_042024<-extract_reduce(AMFR, base) #bo
+
+AMFR<- AM %>% mutate(RI = case_when(Date>"2024-05-15"~ 2))
+AMFR_052024<-extract_reduce(AMFR, base) #bo
+
+
+AM_tbl<-rbind(AMFR_072022,AMFR_092022,AMFR_022023,AMFR_072023,AMFR_092023,
+              AMFR_012024,AMFR_042024,AMFR_052024)
 AM_tbl$ID<-'AM'
 AM_tbl$num<-6
-AM_tbl$IF <- c('bo','bo','rev','bo','bo','rev')
+AM_tbl$IF <- c('bo','bo','rev','bo','bo','rev','bo','bo')
 
-##########################
-#AM mod Reduction#############
-#########################
+#AM mod Reduction######################################
 
 AMmod<-AM %>% filter(depthID != "high")
 
@@ -258,9 +266,7 @@ AMmod_tbl$ID<-'AM'
 AMmod_tbl$num<-6
 AMmod_tbl$IF <- c("h",'h','h','h','h','h')
 
-##########################
-#LF Reduction#############
-#########################
+#LF Reduction######################################
 
 LF<- LF %>% mutate(depthID = case_when(
   depth<0.4 ~ "low",
@@ -268,78 +274,72 @@ LF<- LF %>% mutate(depthID = case_when(
   depth>=0.6 ~ "high"))
 
 LFFR<- LF %>% mutate(RI = case_when(Date> "2022-08-01" & Date<"2022-10-01"~ 2))
-LF_092023<-extract_reduce(LFFR)
+base<-baseline(LFFR)
+LF_092023<-extract_reduce(LFFR,base)
 
 LFFR<- LF %>% mutate(RI = case_when(Date> "2023-01-16" & Date<"2023-04-01"~ 2))
-LF_032023<-extract_reduce(LFFR)
+base<-baseline(LFFR)
+LF_032023<-extract_reduce(LFFR,base)
 
 LFFR<- LF %>% mutate(RI = case_when(Date> "2023-06-10" & Date<"2023-07-30"~ 2))
-LF_062023<-extract_reduce(LFFR)
+base<-baseline(LFFR)
+LF_062023<-extract_reduce(LFFR,base)
+
+LFFR<- LF %>% mutate(RI = case_when(Date> "2023-11-01"~ 2)) %>% filter(RI==2)
+base<-baseline(LFFR)
+
+LFFR<-LFFR %>% mutate(RI = case_when(Date>"2023-11-20" & Date< '2024-03-25'~ 2))
+LFFR_012024<-extract_reduce(LFFR,base)
+
+LFFR<-LFFR %>% mutate(RI = case_when(Date>'2024-03-25' & Date<'2024-05-15'~ 2))
+LFFR_042024<-extract_reduce(LFFR,base) #rev
+
+LFFR<-LFFR %>% mutate(RI = case_when(Date>'2024-05-15'~ 2))
+LFFR_052024<-extract_reduce(LFFR,base) #rev
 
 
-
-
-LFFR_012024<- LF %>% mutate(RI = case_when(Date> "2023-11-01"~ 2)) %>% filter(RI==2)
-LFFR_prior<-LFFR_012024 %>% filter(depthID=='low')
-  
-LFFR_012024<- LFFR_012024 %>% mutate(RI = case_when(Date> "2024-01-01" & Date< '2024-04-10'~ 2))
-LFFR_012024_h<-extract_reduce_dbl(LFFR_012024,LFFR_prior)
-
-LFFR_012024<- LFFR_012024 %>% mutate(RI = case_when(Date> "2024-04-10" & Date< '2024-05-10'~ 2))
-LFFR_012024_FR1<-extract_reduce_dbl(LFFR_012024,LFFR_prior)
-
-LFFR_012024<- LFFR_012024 %>% mutate(RI = case_when(Date> "2024-05-10"~ 2))
-LFFR_012024_FR2<-extract_reduce_dbl(LFFR_012024,LFFR_prior)
-
-
-LF_tbl<-rbind(LF_092023,LF_032023,LF_062023,LFFR_012024_h, LFFR_012024_FR1, LFFR_012024_FR2)
+LF_tbl<-rbind(LF_092023,LF_032023,LF_062023,LFFR_012024,LFFR_042024,LFFR_052024)
 LF_tbl$ID<-'LF'
 LF_tbl$num<-3
 LF_tbl$IF <- c("h","h",'h','h','rev','rev')
 
-
-
-##########################
-#ID Reduction#############
-##########################
+#ID Reduction######################################
 
 ID<- ID %>% mutate(depthID = case_when(
   depth<1.1 ~ "low",
   depth>1.1 & depth<1.5 ~ "moderate",
   depth>=1.5 ~ "high"))
 
-IDFR<- ID %>% mutate(RI = case_when(Date<"2023-04-30"~ 2))
-IDFR_032023<-extract_reduce(IDFR)
+IDFR<- ID %>% mutate(RI = case_when(Date>"2022-12-30"&Date<"2023-04-30"~ 2))
+base<-baseline(IDFR)
+IDFR_032023<-extract_reduce(IDFR, base)
 
-IDFR_F23<- ID %>% mutate(RI = case_when(Date>"2023-04-30" & Date< "2023-11-05"~ 2))%>% filter(RI==2)
-IDFR_F23_prior<-IDFR_F23 %>% filter(depthID=='low')
+IDFR<- ID %>% mutate(RI = case_when(Date>"2023-05-10" & Date< "2023-11-05"~ 2))%>% filter(RI==2)
+base<-baseline(IDFR)
 
-IDFR_F23t<-IDFR_F23 %>% mutate(RI = case_when(Date>'2023-06-17' & Date<'2023-08-01'~2))
-IDFR_072023<-extract_reduce_dbl(IDFR_F23t,IDFR_F23_prior)
+IDFR<-ID %>% mutate(RI = case_when(Date>'2023-06-17' & Date<'2023-08-01'~2))
+IDFR_072023<-extract_reduce(IDFR, base)
 
-IDFR_F23t<-IDFR_F23 %>% mutate(RI = case_when(Date>'2023-08-01' & Date<'2023-08-28'~2))
-IDFR_082023<-extract_reduce_dbl(IDFR_F23t,IDFR_F23_prior)
+IDFR<-ID %>% mutate(RI = case_when(Date>'2023-08-01' & Date<'2023-08-28'~2))
+IDFR_082023<-extract_reduce(IDFR, base)
 
-IDFR_F23t<-IDFR_F23 %>% mutate(RI = case_when(Date>'2023-08-28' & Date<'2023-10-05'~2))
-IDFR_092023<-extract_reduce_dbl(IDFR_F23t,IDFR_F23_prior)
-
-
+IDFR<-ID %>% mutate(RI = case_when(Date>'2023-08-28' & Date<'2023-10-05'~2))
+IDFR_092023<-extract_reduce(IDFR, base)
 
 
-IDFR_W24<- ID %>% mutate(RI = case_when(Date>"2023-11-05" ~ 2))%>% filter(RI==2)
-IDFR_W24_prior<-IDFR_W24 %>% filter(depthID=='low')
 
-IDFR_W24t<-IDFR_W24 %>% mutate(RI = case_when(Date<'2024-02-28'~2))
-IDFR_012024<-extract_reduce_dbl(IDFR_W24t,IDFR_W24_prior)
+IDFR<- ID %>% mutate(RI = case_when(Date>"2023-11-05" ~ 2))%>% filter(RI==2)
+base<-baseline(IDFR)
 
-IDFR_W24t<-IDFR_W24 %>% mutate(RI = case_when(Date>'2024-03-28' & Date<'2024-5-17'~2))
-IDFR_042024<-extract_reduce_dbl(IDFR_W24t,IDFR_W24_prior)
+IDFR<-ID %>% mutate(RI = case_when(Date>"2023-11-05" & Date<'2024-02-28'~2))
+IDFR_012024<-extract_reduce(IDFR, base)
 
-IDFR_W24t<-IDFR_W24 %>% mutate(RI = case_when(Date>'2024-5-17'~2))
-IDFR_062024<-extract_reduce_dbl(IDFR_W24t,IDFR_W24_prior)
+IDFR<-ID %>% mutate(RI = case_when(Date>'2024-03-28' & Date<'2024-5-17'~2))
+IDFR_042024<-extract_reduce(IDFR, base)
 
-# IDFRcheck<-filter(IDFR_W24t,RI==2 )
-# ggplot(IDFRcheck, aes(Date)) +geom_line(aes(y=depth, color=depthID))
+IDFR<-ID %>% mutate(RI = case_when(Date>'2024-5-17'~2))
+IDFR_062024<-extract_reduce(IDFR, base)
+
 
 ID_tbl<-rbind(IDFR_032023,IDFR_072023,IDFR_082023,IDFR_092023,IDFR_012024,
               IDFR_042024, IDFR_062024)
@@ -348,9 +348,7 @@ ID_tbl$num<-2
 ID_tbl$IF <- c("h","h",'h','h','h','h','h')
 
 
-##########################
-#IU Reduction#############
-##########################
+#IU Reduction#######################################
 
 IU<- IU %>% mutate(depthID = case_when(
   depth<1.7 ~ "low",
@@ -358,19 +356,17 @@ IU<- IU %>% mutate(depthID = case_when(
   depth>=3 ~ "high"))
 
 IUFR<- IU %>% mutate(RI = case_when(Date> "2023-11-21"~ 2)) %>% filter(RI==2)
-IUFR_prior<-IUFR%>%filter(depthID=='low')
+base<-baseline(IUFR)
 
-IUFRt<- IUFR %>% mutate(RI = case_when(Date<"2024-03-16"~ 2))
-IUFR_042024<-extract_reduce_dbl(IUFRt, IUFR_prior)
+IUFR<- IU %>% mutate(RI = case_when(Date<"2024-03-16"~ 2))
+IUFR_042024<-extract_reduce(IUFR, base)
 
-IUFRt<- IUFR %>% mutate(RI = case_when(Date>"2024-03-16" & Date<'2024-05-20'~ 2))
-IUFR_052024<-extract_reduce_dbl(IUFRt, IUFR_prior)
+IUFR<- IU %>% mutate(RI = case_when(Date>"2024-03-16" & Date<'2024-05-20'~ 2))
+IUFR_052024<-extract_reduce(IUFR, base)
 
-IUFRt<- IUFR %>% mutate(RI = case_when(Date>'2024-05-20'~ 2))
-IUFR_062024<-extract_reduce_dbl(IUFRt, IUFR_prior)
+IUFR<- IU %>% mutate(RI = case_when(Date>'2024-05-20'~ 2))
+IUFR_062024<-extract_reduce(IUFR, base)
 
-# IUFRcheck<-filter(IUFRt,RI==2 )
-# ggplot(IUFRcheck, aes(Date)) +geom_line(aes(y=depth, color=depthID))
 
 IU_tbl<-rbind(IUFR_042024, IUFR_052024,IUFR_062024)
 IU_tbl$ID<-'IU'
