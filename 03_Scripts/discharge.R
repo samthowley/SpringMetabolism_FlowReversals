cd0ef09de8f289a5c6822b2c66b7d3bdb6302250
Sam Howley <samanthahowley@ufl.edu>
2025-09-12 16:08:00 -0400

    different day

--- 03_Scripts/discharge.R
+++ 03_Scripts/discharge.R
@@ -8,6 +8,11 @@ h.w<-left_join(depth, W, by='ID')
 
 velocity <- read_csv("02_Clean_data/Chem/velocity.csv")
 Q<-left_join(velocity, h.w)%>%mutate(discharge=velocity*depth*w)%>%
-  select(Date, ID, discharge)
+  select(Date, ID, discharge)%>%
+  distinct(Date, ID, .keep_all = T)#%>%
+  filter(!is.na(discharge))%>%
+  mutate(discharge=if_else(discharge<0, 0, discharge))
+
+ggplot(Q, aes(x=Date, y=discharge))+geom_point()+facet_wrap(~ID)
 
 write_csv(Q, "02_Clean_data/Chem/discharge.csv")
